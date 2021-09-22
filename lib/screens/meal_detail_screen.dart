2df3ba0e023eb1ccd7d20de4b1ec03377d1276b7
Naghiyev1 <hnaghiyev@gmail.com>
2021-09-22 17:41:45 +0100

    added Meal Detail Page

---
+++ lib/screens/meal_detail_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../dummy_data.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  static const routeName = '/meal-detail';
+
+  @override
+  Widget build(BuildContext context) {
+    final mealId = ModalRoute.of(context)!.settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${selectedMeal.title}'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            child: Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
